ad973f72af3186dd06c7d63626a8fd6f610ebba9
MTibby92 <MTibby92@gmail.com>
2017-01-23 16:33:01 -0600

    added stormpath to index.js before rendering dom

--- app/index.js
+++ app/index.js
@@ -1,6 +1,10 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 var routes = require('./config/routes')
+var ReactStormpath = require('react-stormpath')
+
+
+ReactStormpath.init()
 
 ReactDOM.render(
 	routes,
