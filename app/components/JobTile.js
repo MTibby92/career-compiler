863ab5b5de84709deed4e3edfc1b8b549bccf218
MTibby92 <MTibby92@gmail.com>
2017-01-24 20:19:54 -0600

    skeleton components for results, decide on modal package to use

---
+++ app/components/JobTile.js
@@ -0,0 +1,20 @@
+var React = require('react')
+var ReactRouter = require('react-router')
+var ReactStormpath = require('react-stormpath')
+
+var JobTile = React.createClass({
+	getInitialState: function() {
+		return {
+			
+		}
+	},
+	render: function() {
+		return (
+			<div className="container">
+				Job Tile
+			</div>
+		)
+	}
+})
+
+module.exports = JobTile
--- app/components/Modal.js
+++ app/components/Modal.js
@@ -2,13 +2,10 @@ var React = require('react')
 var ReactRouter = require('react-router')
 var ReactStormpath = require('react-stormpath')
 
-var JobTile = require('./JobTile')
-var Modal = require('./Modal')
-
 var Authenticated = ReactStormpath.Authenticated
 var NotAuthenticated = ReactStormpath.NotAuthenticated
 
-var Results = React.createClass({
+var Modal = React.createClass({
 	getInitialState: function() {
 		return {
 			
@@ -17,12 +14,10 @@ var Results = React.createClass({
 	render: function() {
 		return (
 			<div className="container">
-				<h3>Results go here</h3>
-				<JobTile />
-				<Modal />
+				Modal
 			</div>
 		)
 	}
 })
 
-module.exports = Results
+module.exports = Modal
