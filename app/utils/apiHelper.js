937a08c3cb4ee9aaf3012a0abd2a79af0b3746ca
MTibby92 <MTibby92@gmail.com>
2017-01-24 19:20:20 -0600

    request works on local host with chrome plugin

--- app/utils/apiHelper.js
+++ app/utils/apiHelper.js
@@ -6,10 +6,9 @@ var authenticJobsKey = keys.authenticJobs
 var helpers = {
 	getAuthenticJobs: function(params) {
 		// var queryString = 'https://authenticjobs.com/api/?api_key=9d6efd898d1e1081a62364b9e7bdaa74&method=aj.jobs.search&format=json'
-		// queryString += '?' + $.param({
+		// queryString += '&' + $.param({
 		// 	'keywords': params[0],
 		// 	'location': params[1]
-		// 	'page': 0
 		// })
 		// return axios.get(queryString)
 		return axios.get('https://authenticjobs.com/api/?', {
@@ -19,8 +18,12 @@ var helpers = {
 				format: 'json',
 				keywords: params[0],
 				location: params[1]
-			},
-			headers: {'Access-Control-Allow-Origin': 'http://localhost:3000'}
+			}
+			// headers: {
+			// 	'Access-Control-Allow-Origin': '*',
+			// 	'Content-Type': 'application/x-www-form-urlencoded',
+			// 	'accept': 'application/json'
+			// }
 		})
 	}
 }
